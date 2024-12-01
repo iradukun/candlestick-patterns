55cf1938bfba95537dd97f8f7a25311fcc2bc070
iradukun <mustaphairadukunda@gmail.com>
2024-12-01 09:52:02 +0200

    feat: added attributes && methods to the class

--- CandleStick.py
+++ CandleStick.py
@@ -6,13 +6,27 @@ class CandleStick:
         self.low = data["low"]
         self.volume = data["volume"]
         self.date = data["date"]
+        # Some extra details
+
+        self.length = self.high - self.low
+        self.bodyLength = abs(self.open - self.close)
+        self.lowerWick = self.__get_lower_wick_length()
+        self.upperWick = self.__get_upper_wick_length()
 
     def __repr__(self):
         return (f"CandleStick(open={self.open}, close={self.close},"
                 f" high={self.high}, low={self.low}, volume={self.volume}")
-  
+
     def is_bullish(self):
         return self.open < self.close
   
     def is_bearish(self):
         return self.open > self.close
+
+    def __get_lower_wick_length(self):
+        """Calculate and return the length of lower shadow or wick."""
+        return (self.open if self.open <= self.close else self.close) - self.low
+
+    def __get_upper_wick_length(self):
+        """Calculate and return the length of upper shadow or wick."""
+        return self.high - (self.open if self.open >= self.close else self.close)
